19341a04621bcab3dd35c026c5359bbf95b87d4e
蒲志斌 <1075057376@qq.com>
2024-07-11 22:45:24 +0800

    Update 03-集合Set.py

--- 02-Python中数据结构有哪些？/03-集合Set.py
+++ 02-Python中数据结构有哪些？/03-集合Set.py
@@ -35,5 +35,36 @@ print(set2)
 set3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 set4 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
 set5 = set3 & set4 # 这里是集合运算符，不是集合方法
-print(set5)
-#
+print("交集：",set5)
+
+# 集合的并集 并集解释：集合A和集合B的并集就是集合A和集合B同时存在的元素，然后输出出来。
+set33 = {1,2,3,4,5}
+set44 = {4,5,6,7,8}
+BJ = set33.union(set44)
+print("并集：",BJ)
+
+# 交集： 同事出现的两个集合中的原
+JJ = set33.intersection(set44)
+print("交集：",JJ)
+
+
+# 差集 ：在第一个集合中但不在第二个集合中的元素
+CJ = set33.difference(set44)
+print("差集：",CJ)
+
+# 对称差集： 只要在一个集合中出现的元素
+DCCJ = set33.symmetric_difference(set44)
+print("对称差集：",DCCJ)
+
+# 集合支持 in-place操作，可以修改原有的集合
+set33 |= set44
+print("in-place |:",set33)
+
+set33 &= set44
+print("in-place &:",set33)
+
+set33 -= set44
+print("in-place -:",set33)
+
+set33 ^= set44
+print("in-place ^:",set33)
